0173cbf35175cb073031ff85820bde1b02f4ff19
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-01-28 02:20:19 -0500

    pass stage 3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -11,15 +12,33 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
+        
         match stream {
             Ok(mut _stream) => {
-                let response = "+PONG\r\n";
-                _stream.write_all(response.as_bytes()).unwrap();
-                _stream.flush().unwrap();
+                handle_connection(&mut _stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+
+fn handle_connection(stream: &mut TcpStream) {
+    loop {
+        let mut buffer = [0; 512];
+        match stream.read(&mut buffer) {
+            Ok(size) => {
+                println!("incoming message: {:?}", std::str::from_utf8(&buffer[..size]));
+                stream.write_all(b"+PONG\r\n").unwrap();
+                stream.flush().unwrap();
+            },
+            Err(e) => {
+                // Handle any error that might occur during read
+                break;
+            },
+        }
+    }
+
 }
